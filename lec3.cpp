f02a46d2f255b759d922805b6366fb84b8c98b3b
MistiGarg11 <gargmisti1105@gmail.com>
2025-09-09 15:00:18 +0530

    Add files via upload
    Conditional Statements and Loops

---
+++ lec3.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+//Conditional statements and loops 
+//Exact same syntax as in C
+/*
+int main(){
+    int age;
+    cout<<"Enter your age :";
+    cin>>age;
+    if (age>=18){
+        cout<<"You can vote";
+    }
+    else{
+        cout<<"Sorry! you are not legal age to vote."
+    }
+    return 0;
+}
+
+//PRACTICE- find character lowercase or uppercase
+int main(){
+     char alpha;
+     cout <<"Enter alphabet :";
+     cin >>alpha;
+     if (alpha>=97 && alpha<=122){
+        cout<<alpha<<" is Lowercase"<<endl;
+     }
+     else if(alpha>=65 && alpha<=90){
+        cout<<alpha<<" is Uppercase"<<endl;
+     }
+     else{
+        cout<<"Invalid entry";
+     }
+     return 0;
+}
+
+//Ternary Operator 
+int main(){
+    int x = (5>4) ? 5-4 : 4/2;
+    cout<<x<<endl;
+  
+    (5>4) ? cout<<"True" : cout<<"False";
+    return 0;
+}
+
+//check if number is prime or not 
+int main(){
+    bool isPrime=true;
+    int num;
+    cout<<"Enter your number : ";
+    cin>>num;
+    for( int i=2;i*i<=num; i++){
+        if ( num % i==0 ){
+            isPrime=0;
+            break;
+        }
+    }
+    if(isPrime==true){
+        cout<<"yes "<<num<<" is prime."<<endl;
+    }
+    else{
+        cout<<"Nope, not prime"<<endl;
+    }
+    return 0;
+}
+*/
